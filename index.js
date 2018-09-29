ad3120e93fd840ab8a6335b164aafe6878cc07c8
Xmader <xmader@outlook.com>
2018-09-29 08:49:43 +0800

    支持更换图片

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cv = document.getElementById('cv');
 const ctx = cv.getContext('2d');
 
 const img = document.getElementById('img1');
+var uploadBtn = document.getElementById("upload-button");
 
 const init = () => {
     const img_width = img.naturalWidth
@@ -13,8 +14,6 @@ const init = () => {
     ctx.drawImage(img, 0, 0, img_width, img_height)
 }
 
-init()
-
 cv.addEventListener("click", (e) => {
     const mo = document.querySelector("#mo")
 
@@ -22,3 +21,18 @@ cv.addEventListener("click", (e) => {
     const height = 200
     ctx.drawImage(mo, e.layerX - 8 - width / 2, e.layerY - 8 - height / 2, width, height)
 })
+
+img.onload = init
+init()
+
+// 获取上传的文件
+const getFile = () => {
+    var reader = new FileReader();
+    reader.readAsDataURL(uploadBtn.files[0]);
+
+    reader.onload = function () {
+        img.src = reader.result; // 是一个base64 Data URL字符串
+    }
+}
+
+uploadBtn.onchange = getFile
