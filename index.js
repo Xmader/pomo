03e0fdde72f247909a7719677ec790d406a7a87c
Xmader <xmader@outlook.com>
2018-09-29 15:53:17 +0800

    泼墨

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const cv = document.getElementById('cv');
 const ctx = cv.getContext('2d');
 
 const img = document.getElementById('img1');
-var uploadBtn = document.getElementById("upload-button");
+const upload_btn = document.getElementById("upload-button");
+const save_btn = document.getElementById("save-button");
 
 const init = () => {
     const img_width = img.naturalWidth
@@ -28,11 +29,30 @@ init()
 // 获取上传的文件
 const getFile = () => {
     var reader = new FileReader();
-    reader.readAsDataURL(uploadBtn.files[0]);
+    reader.readAsDataURL(upload_btn.files[0]);
 
     reader.onload = function () {
         img.src = reader.result; // 是一个base64 Data URL字符串
     }
 }
 
-uploadBtn.onchange = getFile
+upload_btn.onchange = getFile
+
+// 保存画板
+let saved = false
+save_btn.onclick = (e) => {
+
+    if (!saved) {
+        cv.toBlob(function (blob) {
+            url = URL.createObjectURL(blob);
+            // URL.revokeObjectURL(url);
+            e.target.value = "再点一下就可以另存为到本地了"
+            e.target.parentElement.href = url;
+            e.target.parentElement.download = "泼墨画板.png"
+        });
+        saved = true
+    }
+    else{
+        e.target.value = "保存画板"
+    }
+}
