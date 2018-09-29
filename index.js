e643f9dd9184a6500d484d8ef0ea48da71cddef4
Xmader <xmader@outlook.com>
2018-09-29 08:35:21 +0800

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const cv = document.getElementById('cv');
+const ctx = cv.getContext('2d');
+
+const img = document.getElementById('img1');
+
+const init = () => {
+    const img_width = img.naturalWidth
+    const img_height = img.naturalHeight
+
+    cv.width = img_width
+    cv.height = img_height
+
+    ctx.drawImage(img, 0, 0, img_width, img_height)
+}
+
+init()
+
+cv.addEventListener("click", (e) => {
+    const mo = document.querySelector("#mo")
+
+    const width = 40
+    const height = 50
+    ctx.drawImage(mo, e.layerX - 8 - width / 2, e.layerY - 8 - height / 2, width, height)
+})
